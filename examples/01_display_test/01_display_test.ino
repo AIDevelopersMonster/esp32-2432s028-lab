de91af3da48d348aeda00fe12ae71ae9cace9646
AIDevelopersMonster <t89257409519@gmail.com>
2026-08-07 13:29:19 +0300

    Update 01_display_test.ino

--- examples/01_display_test/01_display_test.ino
+++ examples/01_display_test/01_display_test.ino
@@ -1,7 +1,11 @@
+#include <SPI.h>
 #include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
 #include <CYD_Board.h>
 
 TFT_eSPI tft;
+SPIClass touchSpi(VSPI);
+XPT2046_Touchscreen touch(cyd::TOUCH_CS_PIN, cyd::TOUCH_IRQ_PIN);
 
 void setup() {
   Serial.begin(115200);
@@ -10,22 +14,32 @@ void setup() {
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLACK);
-
   tft.setTextColor(TFT_YELLOW, TFT_BLACK);
-  tft.drawString("ESP32-2432S028R", 18, 20, 4);
+  tft.drawString("XPT2046 raw touch test", 12, 12, 2);
+  tft.setTextColor(TFT_LIGHTGREY, TFT_BLACK);
+  tft.drawString("Touch the screen and watch X/Y/Z", 12, 36, 2);
 
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.drawString("ILI9341 display test", 18, 62, 2);
-
-  const uint16_t colors[] = {TFT_RED, TFT_GREEN, TFT_BLUE, TFT_CYAN, TFT_MAGENTA};
-  for (int i = 0; i < 5; ++i) {
-    tft.fillRect(18 + i * 58, 100, 48, 48, colors[i]);
-  }
-
-  tft.drawRect(12, 12, 296, 216, TFT_DARKGREY);
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.drawString("Display initialized successfully", 18, 180, 2);
+  touchSpi.begin(cyd::TOUCH_CLK_PIN,
+                 cyd::TOUCH_MISO_PIN,
+                 cyd::TOUCH_MOSI_PIN,
+                 cyd::TOUCH_CS_PIN);
+  touch.begin(touchSpi);
+  touch.setRotation(1);
 }
 
 void loop() {
+  if (!touch.touched()) {
+    delay(20);
+    return;
+  }
+
+  const TS_Point point = touch.getPoint();
+  Serial.printf("x=%d y=%d z=%d\n", point.x, point.y, point.z);
+
+  tft.fillRect(12, 75, 296, 40, TFT_BLACK);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.setCursor(12, 78, 4);
+  tft.printf("X:%4d Y:%4d Z:%4d", point.x, point.y, point.z);
+
+  delay(60);
 }
